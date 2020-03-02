bf947e2d8f820f8cfc31b86760c498b91d382e29
Rafael Marques <rafael-w5@hotmail.com>
2020-03-02 13:03:50 -0300

    your commit message

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-var num1 = document.getElementById('txt1');
-        var num2 = document.getElementById('txt2');
 
-        function calcular() {
+function executar() {
+// buscando o texto digitado
+var texto = document.getElementById('txtItem').value;
 
-            alert(parseInt(num1.value) + parseInt(num2.value));
-        }
+//buscando a div
+    var div = document.querySelector("div.lista");
+//criando a lista
+    var li = document.createElement('li');
+    var ul = document.createElement('ul');
+// adcionando o texto na lista
+   li.appendChild(document.createTextNode(texto));
+// adicionando a lista na div
+
+var excluir = document.createElement('a')
+var textoLink = document.createTextNode(" - Excluir");
+excluir.setAttribute('href', 'javascript:excluirItem(ul)');
+excluir.appendChild(textoLink);
+
+li.appendChild(excluir);
+ul.appendChild(li);
+    div.appendChild(ul);
+
+   
+}
+
+function excluirItem(itemLista){
+
+    console.log(itemLista);
+    //var elemento_pai = document.body;
+    //elemento_pai.appendChild(itemLista);
+
+   
+
+
+}
