afc3a8d081fedf9c454d3a54e8b314de2c801b64
Rafael Marques <rafael-w5@hotmail.com>
2020-03-02 09:57:06 -0300

    adcionando arquivo app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var num1 = document.getElementById('txt1');
+        var num2 = document.getElementById('txt2');
+
+        function calcular() {
+
+            alert(parseInt(num1.value) + parseInt(num2.value));
+        }
